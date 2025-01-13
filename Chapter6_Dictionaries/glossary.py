3b4debcf926e4ffb252c232621351509303812c0
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2025-01-12 20:18:44 -0500

    added for loop to iterate over a dictionary

--- Chapter6_Dictionaries/glossary.py
+++ Chapter6_Dictionaries/glossary.py
@@ -11,9 +11,9 @@ print(programming_words['if_statement'])
 # Looping through the dictionary using items which prints the key and value
 for key, value in programming_words.items():
     print(f"\nKey: {key}")
-    print(f"\Value: {value}")
+    print(f"Value: {value}")
 
 # Looping through the dictionary using keys() which prints the key of a dictionary.
 #The default behavior of looping through dictionaries is to print the keys only. So you dont have to use the keys() method if you dont want to.
-for program in programming_words.keys():
-    print(program)
+for key, value in programming_words.items():
+    print(f"\n{key}: {value}")
