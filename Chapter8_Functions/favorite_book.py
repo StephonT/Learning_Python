6c169cface9aa08d60d3cb4b798584de40bac42d
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2025-01-24 23:27:21 -0500

    practicing using functions

---
+++ Chapter8_Functions/favorite_book.py
@@ -0,0 +1,4 @@
+def favorite_book(title,author):
+    print(f"One of my favorite books is {title.title()} by {author.title()}.")
+    
+favorite_book("Will", "Will Smith")
---
+++ Chapter8_Functions/message.py
@@ -0,0 +1,6 @@
+#Simple function
+
+def display_message():
+    print("I am learning how to create functions in this chapter.")
+    
+display_message()
