c8b50d30c093a8e0db3943f60bbcbdf80e8757d5
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2025-02-12 19:16:22 -0500

    practice with functions

---
+++ Chapter8_Functions/large_shirts.py
@@ -0,0 +1,6 @@
+def make_shirts(size='large',text='I love Python'):
+  print(f"Shirt Size: {size}")
+  print(f"Shirt Text: {text}")
+  
+make_shirts()
+    
---
+++ Chapter8_Functions/tshirt.py
@@ -0,0 +1,5 @@
+def make_shirt(size,text):
+    print(f"Shirt size: {size}")
+    print(f"Shirt text: {text}")
+    
+make_shirt('XL', 'I love Basketball.')
